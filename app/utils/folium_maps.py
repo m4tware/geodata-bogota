4ba909065cdefde36c69f47ea1205ef60ddd4594
elmateito <mateo.se.arias@gmail.com>
2025-07-15 23:21:10 -0500

    map tile implementation, docstring

--- app/utils/folium_maps.py
+++ app/utils/folium_maps.py
@@ -1,13 +1,34 @@
-from folium import Map, Marker, Icon, GeoJson, FeatureGroup, GeoJsonTooltip, LayerControl
+from folium import Map, Marker, Icon, GeoJson, FeatureGroup, GeoJsonTooltip, LayerControl, TileLayer
 
 def generate_map(*gdf_to_map):
     """
     Each gdf_to_map element must be a tuple:
         >>> (GeoDataFrame, [fields], [aliases], color, layer_name)
     """
-    m = Map(location = [4.65, -74.1], 
-        zoom_start = 11.5
+
+    m = Map(
+        location = [4.65, -74.1], 
+        zoom_start = 11.5,
+        tiles = None
     )
+
+    """
+    Tiles (BaseMap)
+    ---------------
+    tiles = 'https://tiles.stadiamaps.com/tiles/alidade_satellite/{z}/{x}/{y}{r}.png'
+        * provider Class name also can be used: 
+            >>> tiles = 'Stadia.AlidadeSatellite'
+    attr = 'attr string'
+    """
+
+    tiles = 'Stadia.AlidadeSatellite'
+    attr = '&copy; CNES, Distribution Airbus DS, © Airbus DS, © PlanetObserver (Contains Copernicus Data) | &copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+
+    TileLayer(
+        tiles = tiles,
+        attr = attr,
+        control = False,
+    ).add_to(m)
 
     #Marker(
     #    location = [4.65, -74.1],
@@ -24,6 +45,6 @@ def generate_map(*gdf_to_map):
         ).add_to(fg)
         fg.add_to(m)
 
-    LayerControl(collapsed = False).add_to(m)
+    LayerControl(collapsed = False, position = 'bottomright').add_to(m)
 
     return m._repr_html_()
