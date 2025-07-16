5569bab31a3faa4876728f2099ca6f9e607f48af
elmateito <mateo.se.arias@gmail.com>
2025-07-15 23:27:21 -0500

    router data refactorized, test of data fetching, docstring

--- app/routers/cifras.py
+++ app/routers/cifras.py
@@ -1,28 +1,62 @@
 from fastapi import APIRouter, Request
 from fastapi.responses import HTMLResponse
+from geopandas import read_file, GeoDataFrame
 
-from config import DAI_URL, IRL_URL, QUERY
+from config import DAI_LAYER, IRL_LAYER
 from utils.gpd_utils import get_layer_data
-from utils.arcgis_api import fetch_data
+from utils.arcgis_api import fetch_data, get_outfields, get_query_res #temp utils, just fot testing
 from utils.folium_maps import generate_map
 from utils.templates_dir import templates
+from utils.plotly_graphs import generate_bar
 
-Cifras_Router = APIRouter(prefix='/mapas')
+Cifras_Router = APIRouter(prefix='/cifras')
 
-LAYER_DAI = f'{DAI_URL}{QUERY}'
-DAI_DATA = get_layer_data(LAYER_DAI)
+DAI_METADATA = DAI_LAYER['metadata']
+DAI_QUERY    = DAI_LAYER['query']
+DAI_PREFIX   = 'CMHP2'
 
-LAYER_IRL = f'{IRL_URL}{QUERY}'
-IRL_DATA = get_layer_data(LAYER_IRL)
+IRL_METADATA = IRL_LAYER['metadata']
+IRL_QUERY    = IRL_LAYER['query']
+IRL_PREFIX   = 'CMH2'
+
+DAI_outfields = get_outfields(DAI_METADATA, DAI_PREFIX)
+dai_response = get_query_res(DAI_QUERY, DAI_outfields, True)
+DAI_DATA = get_layer_data(dai_response)
+
+IRL_outfields = get_outfields(IRL_METADATA, IRL_PREFIX)
+irl_response = get_query_res(IRL_QUERY, IRL_outfields, True)
+IRL_DATA = get_layer_data(irl_response)
 
 @Cifras_Router.get('/dai', name='get_dai')
 def get_dai():
-    return fetch_data(LAYER_URL) # displaying fetched raw JSON from http request
+    return fetch_data(dai_response) # displaying fetched raw JSON from http request
 
-@Cifras_Router.get('/cifras', response_class=HTMLResponse)
+@Cifras_Router.get('/test_outfields', name='get_dai')
+def get_dai():
+    outfields = get_outfields(DAI_METADATA, DAI_PREFIX)
+    return outfields
+
+@Cifras_Router.get('/test_query', response_class=HTMLResponse, name='get_dai')
+def get_dai(req: Request):
+    outfields = get_outfields(DAI_METADATA, DAI_PREFIX)
+    layer_query_response = get_query_res(DAI_QUERY, outfields, True)
+    data = get_layer_data(layer_query_response)
+
+    return templates.TemplateResponse('home.html', {'request': req})
+
+@Cifras_Router.get('/mapas', response_class=HTMLResponse, name='map_cifras')
 def map_cifras(req: Request):
+    """
+    Return an HTML template, where the GeoDataFrame info is displayed as a:
+    - Folium map
+    - Plotly Bar graph object 
+    """
+
     f_map = generate_map(
         (DAI_DATA, ['CMNOMLOCAL', 'CMHP25CONT'], ['Localidad', ' Hurto a Personas - 2025'], 'green', 'Delitos'),
         (IRL_DATA, ['CMNOMLOCAL', 'CMH25CONT'], ['Localidad', 'Llamadas por Hurto a Personas - 2025'], 'purple', 'Llamadas')
     )
-    return templates.TemplateResponse('cifras_map.html', {'request': req, 'map': f_map})
+
+    p_graph = generate_bar(DAI_DATA, IRL_DATA)
+
+    return templates.TemplateResponse('cifras_map.html', {'request': req, 'map': f_map, 'graph': p_graph})
