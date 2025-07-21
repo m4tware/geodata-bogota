a3cc3d14be99fb4a0c9566183b3d441ee3bf1e8d
elmateito <mateo.se.arias@gmail.com>
2025-07-21 00:08:38 -0500

    implemented merging gdfs with shared fields

--- app/utils/gpd_utils.py
+++ app/utils/gpd_utils.py
@@ -1,4 +1,4 @@
-from geopandas import read_file, GeoDataFrame
+from geopandas import read_file
 from functools import lru_cache
 
 from utils.arcgis_api import fetch_data
@@ -8,7 +8,7 @@ def data_to_gdf(api_data):
     Create a GDF from the passed url from the API router
     ----------
     returns:
-        GeoDataFrame
+        type: GeoDataFrame
     """
     data = fetch_data(api_data, req_dataframe_url=True)
     if data: return read_file(data)
@@ -19,3 +19,20 @@ def get_layer_data(layer_url):
     Cached GeoDataFrame, avoiding ArcGIS requests overheating
     """
     return data_to_gdf(layer_url) # from URL to geodataframe → data to be manipulated with gpd 
+
+def merge_gdf(base_gdf, extra_gdf):
+    """
+    Merge two different GeoDataFrames that share common properties such as:
+    - ['CMIULOCAL', 'CMNOMLOCAL']: both properties(or ArcGIS fields) share the same values.
+    - 'geometry': in some cases, both layers may use the same geometry (this column can be ommitted on: utils.arcgis_api → get_query_res())
+    With this, just 1 GeoDataFrame is used for Data Visualization in Folium and Plotly
+    e.g:
+        >>> (layer_data, extra_layer_data)
+    ----------
+    returns:
+        type: GeoDataFrame
+    """
+    merging = base_gdf.merge(extra_gdf, on=['CMIULOCAL', 'CMNOMLOCAL'], how='inner', suffixes=['', '_right'])
+    merged = merging.drop(columns='geometry_right')
+
+    return merged
